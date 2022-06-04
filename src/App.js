b494c7e7135d780c5fc723be5b0817539e524788
Aditya Thorat <thorat.aditya@outlook.com>
2022-06-04 18:28:32 +0530

    complete slider

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Slider from './components/Slider'
 
+import packageInfo from './data/data.json';
+const banner = packageInfo.banner;
+
 function App() {
     return (
         <Router>
             <PreNavbar />
             <Navbar />
-            <Slider start={}/>
+            <Slider start={banner.start}/>
         </Router>
     );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Slider = ({start}) => {
     return (
-        <Carousel>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src=""
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+        <Carousel fade>
+            {start.map((item) => (
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={item}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                ))
+            }
         </Carousel>
     )
 }
