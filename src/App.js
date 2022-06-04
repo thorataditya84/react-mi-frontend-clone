e3e823d8a07f6396d7cfe4ecf06072c65fdfedf3
Aditya Thorat <thorat.aditya@outlook.com>
2022-06-04 20:22:14 +0530

    complete Navbar component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import data from './data/data.json';
 import {BrowserRouter as Router} from 'react-router-dom';
 import PreNavbar from './components/PreNavbar';
+import Navbar from './components/Navbar';
 
 
 function App() {
     return (
         <Router>
             <PreNavbar />
+            <Navbar />
         </Router>
     );
 }
