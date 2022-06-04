7de942b4e56a70e9376a349dcb09e1b3b928edb0
Aditya Thorat <thorat.aditya@outlook.com>
2022-06-04 20:08:24 +0530

    complete Slider component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
+import data from './data/data.json';
+import {BrowserRouter as Router} from 'react-router-dom';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom';
 import Slider from './components/Slider'
+import Offers from './components/Offers'
 
-import packageInfo from './data/data.json';
-const banner = packageInfo.banner;
+
+
 
 function App() {
     return (
         <Router>
             <PreNavbar />
             <Navbar />
-            <Slider start={banner.start}/>
+            <Slider start={data.banner.start}/>
+            <Offers offer={data.offer} />
         </Router>
     );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,14 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Slider = ({start}) => {
     return (
         <Carousel fade>
-            {start.map((item) => (
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={item}
-                        alt="First slide"
-                    />
-                </Carousel.Item>
+            {
+                start.map((item) => (
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={item}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
                 ))
             }
         </Carousel>
